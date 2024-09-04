4e7acbea44fa859b97f4346c3a610cd55aeeacb3
huipadyam <160405935+huipadyam@users.noreply.github.com>
2024-09-05 00:54:50 +0900

    Update PublicGroupsList.js // 이미지 경로 수정

--- main/src/pages/group/PublicGroupsList.js
+++ main/src/pages/group/PublicGroupsList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./PublicGroupsList.css";
-import imageUrl from "../../img/Img1.png";
+import imageUrl from "../img/Img1.png";
 import flower from "../../img/flower.png";
 
 const BASE_URL = "http://3.39.56.63";
